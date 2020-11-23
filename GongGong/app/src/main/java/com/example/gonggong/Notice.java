67533f4dbcac776cd6d27b710171f4beb0e44da4
Park Jong Ho <57793298+HoJongPARK@users.noreply.github.com>
2020-11-23 17:05:10 +0900

    [GongGong 13]
    [GongGong 1]
    1. 프로젝트 생성
    [GongGong 2]
    1.splash theme 생성
    [GongGong 3]
    1.MainActivity 내비게이션 뷰 생성
    2.내비게이션 뷰에 해당하는 프래그먼트 생성
    [GongGong 4]
    1. 플로팅 액션 버튼 생성
    2. 플로팅 버튼 리스너 생성
    [GongGong 5]
    1. 구글 맵 생성
    2. 구글 맵 급식카드 가맹점 마커추가
    3. 위치 권한 획득& 현재 위치정보
    [GongGong 6]
    1. 마커 클릭 시 Bottom Dialog 생성
    2. Bottom Dialog에 시설 상세정보 표시
    3. 상세정보 보기 액티비티 개발 예정
    [GongGong 7]
    1. 공공데이터 현재 주소 기준으로 데이터 로딩(최적화)
    2. 기타 오류 수정
    [GongGong 8]
    1. 시설 상세보기 레이아웃 구현 완료
    [GongGong 9]
    1. 시설별 상세보기 구현
    2. 길 찾기 버튼 추가 - 외부 지도 어플과 연결, 경로를 찾는다.
    [GongGong 10]
    1.내부 SQL DB 생성 (즐겨찾기 테이블)
    2.시설별 마커 이미지 변경
    3.DB를 활용한 즐겨찾기 구현
    [GongGong 11]
    1. 스레드를 활용한 데이터 로딩 최적화
    2. 즐겨찾기 삭제 기능 구현 중
    [GongGong 12]
    1.즐겨찾기 삭제 기능 구현
    2.즐겨찾기 동작 방식 변경
    [GongGong 13]
    1.공지사항 로딩 개선
    2.공지사항 로딩 중 Progress Dialog 표시

--- GongGong/app/src/main/java/com/example/gonggong/Notice.java
+++ GongGong/app/src/main/java/com/example/gonggong/Notice.java
@@ -1,5 +1,7 @@
 package com.example.gonggong;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import androidx.fragment.app.Fragment;
 
@@ -17,9 +20,10 @@ import org.jsoup.select.Elements;
 import org.w3c.dom.NodeList;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
-public class Notice extends Fragment {
+public class Notice extends Fragment implements Runnable {
     ListView listview;
     NoticeViewAdapter adapter;
     Elements titleContents, centerContents, dateContents, title;
@@ -28,10 +32,16 @@ public class Notice extends Fragment {
     private static String[] titles = new String[10];
     private static String[] dates = new String[10];
     private static String[] links = new String[10];
-
+    private static com.example.gonggong.ProgressDialog progressDialog;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        Thread th=new Thread(Notice.this);
+        progressDialog=new ProgressDialog(getContext());
+        progressDialog.setCancelable(false);
+        progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        progressDialog.show();
+        th.start();
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,20 +51,6 @@ public class Notice extends Fragment {
         adapter = new NoticeViewAdapter();
         listview = (ListView) v.findViewById(R.id.noticeListView);
         listview.setAdapter(adapter);
-        try {
-            getData();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        for(int i=0;i<10;i++){
-            adapter.addItem(nums[i], centers[i], titles[i], dates[i], links[i]);
-        }
-
-        listview.clearChoices() ;
-        adapter.notifyDataSetChanged();
 
         return v;
     }
@@ -63,8 +59,32 @@ public class Notice extends Fragment {
         jsoupAsyncTask.execute();
         title = jsoupAsyncTask.get();
     }
+
+    @Override
+    public void run() {
+        try {
+            getData();
+            for(int i=0;i<10;i++){
+                adapter.addItem(nums[i], centers[i], titles[i], dates[i], links[i]); }
+            Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog.dismiss();
+                    adapter.notifyDataSetChanged();
+                }
+            });
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     private class NoticeJsoup extends AsyncTask<Void, Void, Elements> {
         // AsyncTask객체 생성
+        @Override
+        protected void onPreExecute(){
+
+        }
         @Override
         protected Elements doInBackground(Void... voids) {
             Document doc = null;
@@ -74,35 +94,33 @@ public class Notice extends Fragment {
                 titleContents = doc.select(".left a"); // 셀렉터로 class가 left인 값 중 a태그의 값을 불러옴
                 centerContents = doc.select(".center a");
                 dateContents = doc.select(".listTable td").next().next().next().next();
+                int cnt = 0;
+                for (Element element : titleContents) {
+                    links[cnt] = "https://www.ion.or.kr"+element.attr("onclick").substring(12).replace("');","");
+                    Log.i("title parse end",links[cnt]);
+                    titles[cnt++] = element.text();
+                    if (cnt == 10) // 10개만 파싱
+                        break;
+                }
 
+                cnt = 0;
+                for(Element element : centerContents) {
+                    centers[cnt++] = element.text();
+                    if(cnt == 10) break;
+                }
+
+                cnt = 0;
+                for (Element element : dateContents) {
+                    dates[cnt++] = element.text();
+                    if (cnt == 10) // 10개만 파싱
+                        break;
+                    Log.i("dateparse end",dates[cnt-1]);
+                }
+                return titleContents;
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            int cnt = 0;
-            for (Element element : titleContents) {
-                links[cnt] = "https://www.ion.or.kr"+element.attr("onclick").substring(12).replace("');","");
-                Log.i("#######",links[cnt]);
-                titles[cnt++] = element.text();
-                if (cnt == 10) // 10개만 파싱
-                    break;
-            }
-
-            cnt = 0;
-            for(Element element : centerContents) {
-                centers[cnt++] = element.text();
-                if(cnt == 10) break;
-            }
-
-            cnt = 0;
-            for (Element element : dateContents) {
-                dates[cnt++] = element.text();
-                if (cnt == 10) // 10개만 파싱
-                    break;
-                Log.i("######",dates[cnt-1]);
-            }
-
-            return titleContents;
+            return null;
         }
     }
 }
